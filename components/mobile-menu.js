981dff72b517a8b56721796fd0b84b4a8c1d32a1
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-02-25 12:59:56 +0000

    Add mobile menu

---
+++ components/mobile-menu.js
@@ -0,0 +1,72 @@
+const CreateMobileMenu = ({ id, menuItems }) => {
+	let state = 'closed';
+	const totalItems = menuItems.length;
+
+	const toggle = (event) => {
+		const menuBar = document.querySelector(
+			`#${event.target.parentNode.parentNode.id}`
+		).firstChild;
+		const buttons = menuBar.querySelectorAll('.mobile-menu-button');
+		const toggleButton = menuBar.querySelector('.mobile-menu-toggle');
+
+		if (state === 'closed') {
+			buttons.forEach((button) => {
+				const navButton = button;
+				navButton.style.visibility = 'visible';
+				navButton.style.opacity = 1;
+			});
+			toggleButton.textContent = '-';
+			menuBar.style.backgroundColor = 'rgba(240, 109, 6, 1)';
+
+			state = 'open';
+		} else if (state === 'open') {
+			buttons.forEach((button) => {
+				const navButton = button;
+				navButton.style.opacity = 0;
+				navButton.style.visibility = 'hidden';
+			});
+			toggleButton.textContent = '+';
+			menuBar.style.backgroundColor = 'rgba(240, 109, 6, 0)';
+
+			state = 'closed';
+		}
+	};
+
+	const render = () => {
+		const mobileMenu = document.createElement('div');
+		mobileMenu.classList.add('mobile-menu');
+		mobileMenu.setAttribute('id', id);
+
+		const mobileMenuBar = document.createElement('nav');
+		mobileMenuBar.classList.add('mobile-menu-bar');
+		mobileMenu.appendChild(mobileMenuBar);
+
+		mobileMenuBar.innerHTML = '';
+		menuItems.forEach((item) => {
+			if (menuItems.indexOf(item) === Math.floor(totalItems / 2)) {
+				const toggleButton = document.createElement('button');
+				toggleButton.textContent = '+';
+				toggleButton.classList.add('mobile-menu-toggle');
+				toggleButton.addEventListener('click', (event) => {
+					toggle(event);
+				});
+
+				mobileMenuBar.appendChild(toggleButton);
+			}
+
+			const button = document.createElement('button');
+			button.classList.add('mobile-menu-button');
+			button.textContent = item.title;
+			button.style.visibility = 'hidden';
+			mobileMenuBar.appendChild(button);
+		});
+
+		return mobileMenu;
+	};
+
+	return {
+		render,
+	};
+};
+
+export default CreateMobileMenu;
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import CreateDropdownMenu from './components/dropdown-menu.js';
 import CreateImageSlider from './components/image-slider.js';
+import CreateMobileMenu from './components/mobile-menu.js';
 
 const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+const hero = document.querySelector('#hero');
 
 const dropDown1 = CreateDropdownMenu({
-	menuTitle: 'Click Me',
+	menuTitle: 'Nav Item 1',
 	menuItems: [{ title: 'Link1', url: 'http://www.google.com' }],
 });
 
 const dropDown2 = CreateDropdownMenu({
-	menuTitle: 'Click Me 2',
+	menuTitle: 'Nav Item 2',
 	menuItems: [
 		{ title: 'Link 2', url: 'http://www.google.com' },
 		{ title: 'Link 3', url: 'http://www.google.com' },
@@ -26,16 +29,17 @@ const slider1 = CreateImageSlider({
 	],
 });
 
-const slider2 = CreateImageSlider({
-	id: 'slider-2',
-	images: [
-		'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
-		'https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/3:2/w_3329,h_2219,c_limit/1521-WIRED-Cat.jpeg',
-		'https://c.files.bbci.co.uk/151AB/production/_111434468_gettyimages-1143489763.jpg',
+const mobileMenu1 = CreateMobileMenu({
+	id: 'mobile-menu-1',
+	menuItems: [
+		{ title: 'Home', url: '#' },
+		{ title: 'News', url: '#' },
+		{ title: 'Profile', url: '#' },
+		{ title: 'Settings', url: '#' },
 	],
 });
 
-body.appendChild(dropDown1.render());
-body.appendChild(dropDown2.render());
-body.appendChild(slider1.render());
-body.appendChild(slider2.render());
+body.appendChild(mobileMenu1.render());
+nav.appendChild(dropDown1.render());
+nav.appendChild(dropDown2.render());
+hero.appendChild(slider1.render());
